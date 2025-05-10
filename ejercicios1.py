60533d3a1bb135f8cb6777cd82a3bb182590a33e
ErvinGU <ervin.garay+1@gmail.com>
2025-05-10 21:36:09 +0000

    python

--- ejercicios1.py
+++ ejercicios1.py
@@ -1,4 +1,4 @@
-
+import math as m
 
 
 
@@ -24,9 +24,31 @@ print(f"el IMC es : {imc:.2f}") #.2f indica cantidad de decimales. """
 #calculadora de descuento
 #precio original × (porcentaje / 100) = descuento
 
-precio_original = int(input("cual es el precio? : "))
+""" precio_original = float(input("cual es el precio? : "))
 descuento = float(input("cual es el porcentaje de descuento? : "))
 precia_con_descuento = precio_original * (descuento / 100)
 precio_final = precio_original - precia_con_descuento
 
-print(f"precio con descuento es : {precio_final}")
+print(f"precio con descuento es : {precio_final}") """
+
+
+#calculadora promedio de notas
+#x= n1 + n2 + n3 / n
+
+""" nota1 = float(input("nota 1"))
+nota2 = float(input("nota 2"))
+nota3 = float(input("nota 3"))
+
+promedio = (nota1 + nota2 + nota3) / 3 
+
+print(f"el promedio de nota es : {promedio:.2f}") """
+
+
+#area de un triangulo equilater0
+#A = (√3 / 4) * L²
+
+lado = float(input(f"longitud del lado del triangulo"))
+
+area = (m.sqrt(3) / 4) * lado **2
+
+print(f"Area del triangulo es : {area:.1f}")
